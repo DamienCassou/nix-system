7511b2dd23db34ed3910206772cacba1d3d727c2
Damien Cassou <damien@cassou.me>
2024-04-13 17:15:21 +0200

    packages: wrap nodejs packages

--- config/home-manager/dev.nix
+++ config/home-manager/dev.nix
@@ -1,4 +1,20 @@
-{ ... }:
+{ pkgs, lib, ... }:
+
+let
+  nodejs = pkgs.nodejs_20;
+  # Adding libuuid to some node binaries are required by the
+  # "node-canvas" package (used by webdriver.io for visual testing for
+  # example)
+  wrapWithMissingLibraries =
+    binaryFile:
+    pkgs.writeShellScriptBin (baseNameOf binaryFile) ''
+      LD_LIBRARY_PATH="${pkgs.lib.makeLibraryPath [ pkgs.libuuid ]}";
+      export LD_LIBRARY_PATH
+      exec ${binaryFile} "$@";
+    '';
+  node = wrapWithMissingLibraries (lib.getExe nodejs);
+  yarn = wrapWithMissingLibraries (lib.getExe pkgs.yarn);
+in
 {
   imports = [
     ./bash.nix
@@ -11,4 +27,9 @@
   programs.bun = {
     enable = true;
   };
+
+  home.packages = [
+    node
+    yarn
+  ] ++ (with pkgs; [ jwt-cli ]);
 }
