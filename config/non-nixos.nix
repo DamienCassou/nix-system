5f0e44a4ca8ecd8945e953ac1afdde6433960655
Damien Cassou <damien@cassou.me>
2024-11-11 10:14:30 +0100

    config: Delete unused configuration of NIX_PATH
    This doesn't seem necessary anymore. Maybe since I switched to flakes.

--- config/non-nixos.nix
+++ config/non-nixos.nix
@@ -1,31 +1,11 @@
-{ config, lib, ... }:
+{ config, ... }:
 {
   systemd.user.systemctlPath = "/usr/bin/systemctl";
 
   targets.genericLinux.enable = true;
 
-  home.sessionVariables = {
-    # https://nixos.org/nixpkgs/manual/#locales
-    NIX_PATH = "/home/cassou/Documents/projects/nix-system";
-  };
-
-  pam.sessionVariables = {
-    NIX_PATH = "/home/cassou/Documents/projects/nix-system";
-  };
-
-  systemd.user.sessionVariables.NIX_PATH = lib.mkForce "/home/cassou/Documents/projects/nix-system";
-
   home.sessionVariablesExtra = ''
     export INFOPATH="/usr/share/info:${config.home.profileDirectory}/share/info:''${INFOPATH}"
-    export NIX_PATH="/home/cassou/Documents/projects/nix-system"
-  '';
-
-  xsession.profileExtra = ''
-    export NIX_PATH="/home/cassou/Documents/projects/nix-system"
-  '';
-
-  xsession.initExtra = ''
-    export NIX_PATH="/home/cassou/Documents/projects/nix-system"
   '';
 
   imports = [ ./common ];
