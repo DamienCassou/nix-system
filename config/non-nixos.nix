66ab034eb7e88625200474060233fa8fed193d20
Damien Cassou <damien@cassou.me>
2024-11-11 10:05:31 +0100

    Move some notes to README.org

--- config/non-nixos.nix
+++ config/non-nixos.nix
@@ -1,27 +1,3 @@
-# Notes for a new non-NixOS install:
-# - localectl set-x11-keymap us pc105 colemak ctrl:nocaps
-# - Install nitrokey udev rules
-#   https://docs.nitrokey.com/software/nitropy/linux/udev
-# - Deactivate SELINUX
-# - hostnamectl set-hostname luz5
-# - sudo usermod --append --groups dialout,docker,wheel,wireshark,vboxusers,adbusers cassou
-# - Install nix in multi-user mode: https://nixos.org/manual/nix/stable/#sect-multi-user-installation
-# - Install home-manager: https://github.com/nix-community/home-manager/blob/master/README.md
-# - home-manager -f config/non-nixos.nix -I nixpkgs=nixpkgs/ -I home-manager=home-manager/ -b bak1 switch
-# - dnf install lightdm lightdm-gtk i3lock swaylock NetworkManager-strongswan
-# - systemctl disable gdm
-# - systemctl enable lightdm
-# - cp ./i3.desktop /usr/share/xsessions/default.desktop
-# - cp ./sway.desktop to /usr/share/wayland-sessions
-# - reboot
-# - in lightdm, choose the "Default Xsession" session from the menu in the top bar
-# - install i3lock # https://github.com/i3/i3lock/issues/286
-# - install mono # https://www.mono-project.com/download/preview/
-#     dnf install mono-devel mono-complete xsp referenceassemblies-pcl msbuild
-# - install docker (https://docs.docker.com/engine/install/fedora/#install-using-the-repository)
-# - Allow docker to access port 5000 (rider): sudo ufw allow in from any to 172.17.0.1/24 port 5000
-# - Add "127.0.0.1  database.ftgp" and "127.0.0.1   rabbitmq" à /etc/hosts
-# - Install dotnet /home/cassou/configuration/denote/20240619T111823--mettre-à-jour-dotnet__finsit_monitor.org
 { config, lib, ... }:
 {
   systemd.user.systemctlPath = "/usr/bin/systemctl";
