f38c35fba3840cbbba86db4411bddd58ccb5bf51
Damien Cassou <damien@cassou.me>
2024-11-26 13:31:13 +0100

    format

--- config/common/firefox/bookmarks/toolbar.nix
+++ config/common/firefox/bookmarks/toolbar.nix
@@ -13,13 +13,11 @@ _: {
       bookmarks = [
         {
           name = "🤘 Cycle";
-          url =
-            "https://3.basecamp.com/4201305/buckets/11533543/messages/6833162108";
+          url = "https://3.basecamp.com/4201305/buckets/11533543/messages/6833162108";
         }
         {
           name = "Client todos";
-          url =
-            "https://3.basecamp.com/4201305/buckets/15549487/todosets/2378332609";
+          url = "https://3.basecamp.com/4201305/buckets/15549487/todosets/2378332609";
         }
         {
           name = "Client-side tools";
@@ -27,25 +25,21 @@ _: {
         }
         {
           name = "finsit schedule";
-          url =
-            "https://3.basecamp.com/4201305/buckets/11533543/schedules/1692242166";
+          url = "https://3.basecamp.com/4201305/buckets/11533543/schedules/1692242166";
         }
       ];
     }
     {
       name = "mergeable";
-      url =
-        "https://github.com/pulls?q=is%3Aopen+is%3Apr+archived%3Afalse+label%3Amergeable+user%3Aforetagsplatsen";
+      url = "https://github.com/pulls?q=is%3Aopen+is%3Apr+archived%3Afalse+label%3Amergeable+user%3Aforetagsplatsen";
     }
     {
       name = "mine";
-      url =
-        "https://github.com/pulls?q=is%3Aopen+is%3Apr+author%3ADamienCassou+archived%3Afalse";
+      url = "https://github.com/pulls?q=is%3Aopen+is%3Apr+author%3ADamienCassou+archived%3Afalse";
     }
     {
       name = "reviews";
-      url =
-        "https://github.com/pulls?q=is%3Aopen+is%3Apr+review-requested%3ADamienCassou+archived%3Afalse";
+      url = "https://github.com/pulls?q=is%3Aopen+is%3Apr+review-requested%3ADamienCassou+archived%3Afalse";
     }
     {
       name = "monitor";
