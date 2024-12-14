52e355e9bdb6a23b603dd44689ba81b1df5b17ff
Damien Cassou <damien@cassou.me>
2024-12-14 16:19:24 +0100

    Move some dev-related conf to dev/

--- config/common/default.nix
+++ config/common/default.nix
@@ -271,41 +271,14 @@ in
 
     sessionPath = [
       "${home}/.local/bin"
-      config.home.sessionVariables.DOTNET_ROOT
-      "${config.home.sessionVariables.DOTNET_ROOT}/tools"
     ];
 
     sessionVariables = {
       LEDGER_FILE = "${home}/personal/ledger/accounting.hledger";
 
-      CLEAR_CONSOLE = "false";
-
-      # Prevent .NET Core from sending usage statistics:
-      DOTNET_CLI_TELEMETRY_OPTOUT = "true";
-
-      # Tell dotnet where to find the installations
-      DOTNET_ROOT = "${home}/.dotnet";
-
       MAILDIR = "${config.accounts.email.maildirBasePath}";
-
-      # I need this variable to be accessible from desktop
-      # applications, not only from shell sessions. Using
-      # pass-show-password doesn't seem to work for desktop
-      # applications, maybe because gpg isn't ready when the session
-      # variables are initially set.
-      # The error message I get is:
-      # > Value: gpg: public key decryption failed: No such file or directory
-      # > gpg: decryption failed: No such file or directory
-      FINSIT_GITHUB = "$(cat ${lib.elemAt config.nix.nixPath 0}/secrets/FINSIT_GITHUB)";
-
-      JFROG_API_KEY = "$(pass-show-password wk/jfrog.io/token)";
-
-      ESLINT_USE_FLAT_CONFIG = "true";
     };
 
-    extraOutputsToInstall = [
-      "doc"
-      "devdoc"
-    ];
+    extraOutputsToInstall = [ "doc" ];
   };
 }
