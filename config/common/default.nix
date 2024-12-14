1cb028d22a9bc7b62def60bacdfda2c4ca2967fa
Damien Cassou <damien@cassou.me>
2024-12-14 16:40:02 +0100

    Move nix config to a dedicated file

--- config/common/default.nix
+++ config/common/default.nix
@@ -21,6 +21,7 @@ in
     ./gpg.nix
     ./my-packages
     ./my-scripts.nix
+    ./nix.nix
     ./packages.nix
     ./pim
     ./report-changes.nix
@@ -195,43 +196,6 @@ in
   };
 
   fonts.fontconfig.enable = true;
-
-  nix = {
-    package = pkgs.nix;
-    nixPath = [ "${home}/personal/nix-system" ];
-    channels = { };
-    keepOldNixPath = false;
-    settings = {
-      experimental-features = [
-        "flakes"
-        "nix-command"
-      ];
-      substituters = [
-        "https://cache.nixos.org"
-        "https://devenv.cachix.org"
-        "https://emacs-ci.cachix.org"
-        "https://nix-community.cachix.org"
-      ];
-      trusted-public-keys = [
-        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-        "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
-        "emacs-ci.cachix.org-1:B5FVOrxhXXrOL0S+tQ7USrhjMT5iOPH+QN9q0NItom4="
-        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      ];
-    };
-    registry = {
-      "flakes" = {
-        from = {
-          id = "nixpkgs";
-          type = "indirect";
-        };
-        to = {
-          path = "${lib.elemAt config.nix.nixPath 0}/nixpkgs";
-          type = "path";
-        };
-      };
-    };
-  };
 
   home = {
     stateVersion = "24.11";
