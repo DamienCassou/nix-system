fc45e7e3432bad85f26ade708f7b7bd9c9c29ce0
Damien Cassou <damien@cassou.me>
2024-12-14 16:14:13 +0100

    Move definition of certificatesFile to linux/

--- config/linux/default.nix
+++ config/linux/default.nix
@@ -1,8 +1,20 @@
 { ... }:
+let
+  certificatesFile = "/etc/ssl/certs/ca-bundle.crt";
+in
 {
   imports = [
     ./desktop-entries.nix
   ];
+
+  accounts.email.certificatesFile = certificatesFile;
+
+  home = {
+    sessionVariables = {
+      # So git finds the certificates on the woob clone:
+      GIT_SSL_CAINFO = certificatesFile;
+    };
+  };
 
   programs.bash.shellAliases = {
     "dnf-list" = "dnf repoquery --list";
