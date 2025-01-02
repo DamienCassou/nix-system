3784e848c64ff40502316a2d068698ba8df35e07
Damien Cassou <damien@cassou.me>
2025-01-02 08:50:08 +0100

    nixpkgs-review.sh: Fix

--- config/common/my-scripts/nixpkgs-review.sh
+++ config/common/my-scripts/nixpkgs-review.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-cd ~/personal/projects/nix/nixpkgs-master/ || exit 1
+cd ~/personal/projects/nix/nixpkgs/ || exit 1
 
 GITHUB_TOKEN=$(pass-show-password "api.github.com/DamienCassou^nixpkgs-review")
 export GITHUB_TOKEN
 
 options="pr $@ --post-result"
-exec nix-shell -p nixpkgs-review --run "nixpkgs-review $options"
+@nixpkgs-review@ $options
