63eddbae40fe7a5b86d4ce23666df67159d5d8b1
Damien Cassou <damien@cassou.me>
2025-01-08 17:24:04 +0100

    make-token: Move reusable code to utils.js

--- config/common/my-packages/make-token/src/makeToken.js
+++ config/common/my-packages/make-token/src/makeToken.js
@@ -1,12 +1,5 @@
-import { select } from "@inquirer/prompts";
-import { readdir } from "node:fs/promises";
-import { promisify } from "node:util";
-import { execFile as execFileCallback } from "node:child_process";
 import jwt from "jsonwebtoken";
-
-const execFile = promisify(execFileCallback);
-
-const TOKEN_SECRETS_DIR = `${process.env.PASSWORD_STORE_DIR}/ftgp/token-secrets`;
+import { listTargets, chooseTarget, readSecret } from "./utils.js";
 
 async function main() {
   const targets = await listTargets();
@@ -15,45 +8,6 @@ async function main() {
   const token = await makeToken({ secret });
 
   console.log(token);
-}
-
-async function listTargets() {
-  const entries = await readdir(TOKEN_SECRETS_DIR, {
-    withFileTypes: true,
-  });
-
-  const files = entries.filter(
-    (entry) => entry.isFile() && entry.name.endsWith(".foretagsplatsen.se.gpg"),
-  );
-
-  return files.map((file) => file.name.replace(/\.gpg$/, ""));
-}
-
-/**
- * @param {object} params -
- * @param {string[]} params.targets
- */
-function chooseTarget({ targets }) {
-  return select(
-    {
-      message: "Select a target",
-      choices: targets.map((target) => ({ name: target, value: target })),
-    },
-    { output: process.stderr },
-  );
-}
-
-async function readSecret({ target }) {
-  // Decrypt the GPG file at TOKEN_SECRETS_DIR/${target}.gpg by executing gpg
-  // and reading the output.
-  const { stdout } = await execFile(
-    "gpg",
-    ["--decrypt", `${TOKEN_SECRETS_DIR}/${target}.gpg`],
-    { encoding: "utf-8" },
-  );
-
-  // return the first line of output:
-  return stdout.slice(0, stdout.indexOf("\n"));
 }
 
 function makeToken({ secret }) {
---
+++ config/common/my-packages/make-token/src/utils.js
@@ -0,0 +1,43 @@
+import { select } from "@inquirer/prompts";
+import { readdir } from "node:fs/promises";
+import { promisify } from "node:util";
+import { execFile as execFileCallback } from "node:child_process";
+
+const execFile = promisify(execFileCallback);
+
+const TOKEN_SECRETS_DIR = `${process.env.PASSWORD_STORE_DIR}/ftgp/token-secrets`;
+
+export async function listTargets() {
+  const entries = await readdir(TOKEN_SECRETS_DIR, {
+    withFileTypes: true,
+  });
+
+  const files = entries.filter(
+    (entry) => entry.isFile() && entry.name.endsWith(".foretagsplatsen.se.gpg"),
+  );
+
+  return files.map((file) => file.name.replace(/\.gpg$/, ""));
+}
+
+export function chooseTarget({ targets }) {
+  return select(
+    {
+      message: "Select a target",
+      choices: targets.map((target) => ({ name: target, value: target })),
+    },
+    { output: process.stderr },
+  );
+}
+
+export async function readSecret({ target }) {
+  // Decrypt the GPG file at TOKEN_SECRETS_DIR/${target}.gpg by executing gpg
+  // and reading the output.
+  const { stdout } = await execFile(
+    "gpg",
+    ["--decrypt", `${TOKEN_SECRETS_DIR}/${target}.gpg`],
+    { encoding: "utf-8" },
+  );
+
+  // return the first line of output:
+  return stdout.slice(0, stdout.indexOf("\n"));
+}
