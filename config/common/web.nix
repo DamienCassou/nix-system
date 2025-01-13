69dd156bb0e86ce6df8c675c2f2ed6773d5a46d8
Damien Cassou <damien@cassou.me>
2025-01-13 17:05:45 +0100

    chromium: Add instructions

--- config/common/web.nix
+++ config/common/web.nix
@@ -8,6 +8,10 @@
     ];
   };
 
+  # Manual steps:
+  # 1. Open chromium
+  # 2. Go to https://github.com/NeverDecaf/chromium-web-store/releases/latest and download CRX to install extension
+  # 2. Go to https://chromewebstore.google.com/detail/browserpass/naepdomgkenhinolocfifgehidddafch and install
   programs.chromium = {
     enable = true;
     package = config.lib.nixGL.wrap pkgs.ungoogled-chromium;
