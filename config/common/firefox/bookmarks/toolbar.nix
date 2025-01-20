4c78b2ab9b578f25dbc1965fc89f92049db58228
Damien Cassou <damien@cassou.me>
2025-01-20 09:41:04 +0100

    firefox: Update bookmarks

--- config/common/firefox/bookmarks/toolbar.nix
+++ config/common/firefox/bookmarks/toolbar.nix
@@ -2,7 +2,7 @@ _: {
   bookmarks = [
     {
       name = "Project";
-      url = "https://3.basecamp.com/4201305/buckets/40508203/todosets/8184495262";
+      url = "https://3.basecamp.com/4201305/projects/40698660";
     }
     {
       name = "dev-doc";
