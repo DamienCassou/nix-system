e4097a48c090f9fd5d5ae6359d73453150927cdc
Damien Cassou <damien@cassou.me>
2025-02-14 14:47:55 +0100

    Fix signaturepdf

--- flake.nix
+++ flake.nix
@@ -19,7 +19,7 @@
     };
 
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:DamienCassou/home-manager/signaturepdf-fix-xdg";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
