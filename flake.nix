784304041f9a3ef15cf3b3f471f5e8a906cad95d
Damien Cassou <damien@cassou.me>
2025-02-14 14:01:39 +0100

    Move home-manager-config/nix-darwin/ to nix-darwin-config/

--- flake.nix
+++ flake.nix
@@ -80,7 +80,7 @@
       darwinConfigurations = {
         macbook = darwin.lib.darwinSystem {
           modules = [
-            ./config/nix-darwin
+            ./nix-darwin-config
             home-manager.darwinModules.home-manager
             {
               system.configurationRevision = self.rev or self.dirtyRev or null;
