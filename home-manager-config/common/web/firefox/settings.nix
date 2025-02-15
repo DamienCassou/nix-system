cc2e899a272365e7f35ce3e6c8df75d1d9916711
Damien Cassou <damien@cassou.me>
2025-02-15 16:05:05 +0100

    firefox: Stop the beep in case of failed search

--- home-manager-config/common/web/firefox/settings.nix
+++ home-manager-config/common/web/firefox/settings.nix
@@ -4,6 +4,7 @@ _:
   # - https://restoreprivacy.com/firefox-privacy/
   # - https://arkenfox.github.io/gui/
   settings = {
+    accessibility.typeaheadfind.enablesound = false;
     # Privacy
     "privacy.firstparty.isolate" = true;
     # Misc
