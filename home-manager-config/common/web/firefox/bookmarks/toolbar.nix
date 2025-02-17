963f9299a12ebda7126745de2a9c723557225a66
Damien Cassou <damien@cassou.me>
2025-02-17 21:49:12 +0100

    firefox: Update bookmarks

--- home-manager-config/common/web/firefox/bookmarks/toolbar.nix
+++ home-manager-config/common/web/firefox/bookmarks/toolbar.nix
@@ -2,7 +2,7 @@ _: {
   bookmarks = [
     {
       name = "Project";
-      url = "https://3.basecamp.com/4201305/projects/40901843";
+      url = "https://3.basecamp.com/4201305/projects/41106256";
     }
     {
       name = "dev-doc";
