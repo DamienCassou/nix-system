2ef7a458dc8ace2e85fed12ac5cfbf4eeda14259
Damien Cassou <damien@cassou.me>
2025-02-22 09:50:39 +0100

    darwin: Convert file specification to launchd agent

--- home-manager-config/darwin/default.nix
+++ home-manager-config/darwin/default.nix
@@ -11,39 +11,45 @@
         enable-ssh-support
         pinentry-program ${pkgs.lib.getExe pkgs.pinentry_mac}
       '';
-
-      # Make all sessionVariables available to non-shell applications
-      # by adding them to launchd:
-      "Library/LaunchAgents/damien.plist".text =
-        let
-          vars = config.home.sessionVariables;
-          # List of variable names and values looking like (VAR1 VAL1
-          # VAR2 VAL2 …):
-          variableAndValues = (
-            lib.flatten (
-              lib.mapAttrsToList (varName: varValue: [
-                varName
-                varValue
-              ]) vars
-            )
-          );
-        in
-        lib.generators.toPlist { } {
-          Label = "damien";
-          ProgramArguments = [
-            "launchctl"
-            "setenv"
-          ] ++ variableAndValues;
-          RunAtLoad = true;
-        };
     };
 
     sessionPath = [ "/opt/homebrew/bin" ];
+  };
+
+  launchd = {
+    enable = true;
+    agents = {
+      setProfileEnvVariables = {
+        enable = true;
+        config =
+          let
+            vars = config.home.sessionVariables;
+            # List of variable names and values looking like (VAR1 VAL1
+            # VAR2 VAL2 …):
+            variableAndValues = (
+              lib.flatten (
+                lib.mapAttrsToList (varName: varValue: [
+                  varName
+                  varValue
+                ]) vars
+              )
+            );
+          in
+          {
+            ProgramArguments = [
+              "launchctl"
+              "setenv"
+            ] ++ variableAndValues;
+            RunAtLoad = true;
+          };
+      };
+    };
   };
 
   programs.firefox.package = pkgs.firefox-bin;
 
   services = {
     mpd.musicDirectory = "${config.home.homeDirectory}/personal/music/damien";
-    syncthing.enable = true;};
+    syncthing.enable = true;
+  };
 }
