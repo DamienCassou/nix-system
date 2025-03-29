90663dc30fc40241d966b7943f6edc690d3b34dd
Damien Cassou <damien@cassou.me>
2025-03-29 14:15:28 +0100

    flake: Update homedir path

--- flake.nix
+++ flake.nix
@@ -19,7 +19,7 @@
     };
 
     home-manager = {
-      url = "git+file:///Users/cassou/personal/projects/nix/home-manager?ref=system";
+      url = "git+file:///home/cassou/personal/projects/nix/home-manager?ref=system";
       # url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
@@ -35,7 +35,7 @@
     };
 
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    # nixpkgs.url = "git+file:///Users/cassou/personal/projects/nix/nixpkgs?ref=system";
+    # nixpkgs.url = "git+file:///home/cassou/personal/projects/nix/nixpkgs?ref=system";
 
     nixpkgs-firefox-darwin.url = "github:bandithedoge/nixpkgs-firefox-darwin";
 
