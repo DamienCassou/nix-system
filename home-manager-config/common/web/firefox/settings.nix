77244e436d1cc910a934e2fec3c090042865b8c3
Damien Cassou <damien@cassou.me>
2025-07-02 19:41:49 +0200

    firefox: Allow all ports

--- home-manager-config/common/web/firefox/settings.nix
+++ home-manager-config/common/web/firefox/settings.nix
@@ -1,10 +1,12 @@
-_:
-{
+_: {
   # Reference links:
   # - https://restoreprivacy.com/firefox-privacy/
   # - https://arkenfox.github.io/gui/
   settings = {
     accessibility.typeaheadfind.enablesound = false;
+    # By default firefox blocks 5060 which we need for ReportApi
+    # (https://superuser.com/questions/188006/how-to-fix-err-unsafe-port-error-on-chrome-when-browsing-to-unsafe-ports)
+    "network.security.ports.banned.override" = "1-65535";
     # Privacy
     "privacy.firstparty.isolate" = true;
     # Misc
