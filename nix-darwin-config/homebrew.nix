d7b9ef9d86cc8249399848bc84066237fcb6ed66
Damien Cassou <damien@cassou.me>
2025-09-26 09:44:24 +0200

    homebrew: Remove discord as it is blocked now

--- nix-darwin-config/homebrew.nix
+++ nix-darwin-config/homebrew.nix
@@ -11,7 +11,6 @@
       "android-platform-tools"
       "bluesnooze" # deactivate bluetooth while the computer sleeps
       "calibre"
-      "discord"
       "element"
       "gimp"
       "halloy" # Client IRC
