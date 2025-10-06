fa893cc919dfbdb0dbb44585a0f3622da2858877
Damien Cassou <damien@cassou.me>
2025-10-06 15:55:38 +0200

    Reindent

--- home-manager-config/common/packages.nix
+++ home-manager-config/common/packages.nix
@@ -10,81 +10,80 @@ let
   iosevka-aile = pkgs.iosevka-bin.override { variant = "Aile"; };
 in
 {
-  home.packages =
+  home.packages = [
+    aspel
+    iosevka-aile
+  ]
+  ++ (
+    with pkgs;
     [
-      aspel
-      iosevka-aile
+      bc # calculator
+      coreutils-full
+      curl
+      diffutils
+      direnv
+      dmenu
+      dot-language-server
+      easytag
+      ffmpeg-full # for ffplay, used by tmr-sound.el
+      file
+      findutils
+      gawk
+      gnugrep
+      gnumake
+      gnused
+      graphviz
+      stable.haskellPackages.hledger_1_40
+      hunspellDicts.en_US-large
+      hunspellDicts.fr-any
+      hunspellDicts.fr-classique
+      hunspellDicts.fr-moderne
+      imagemagick
+      ispell
+      jq
+      multimarkdown # to preview markdown files
+      ncdu
+      p7zip
+      pandoc
+      pwgen
+      qpdf # to remove passwords from PDF
+      rsync
+      spice
+      sqlite
+      umlet
+      unar # universal unpacker
+      util-linux
+      unzip
+      wget
+      wmname
+      yaml-language-server
+      yarn-bash-completion
+      zbar # to scan QR codes for pass-otp
     ]
-    ++ (
-      with pkgs;
-      [
-        bc # calculator
-        coreutils-full
-        curl
-        diffutils
-        direnv
-        dmenu
-        dot-language-server
-        easytag
-        ffmpeg-full # for ffplay, used by tmr-sound.el
-        file
-        findutils
-        gawk
-        gnugrep
-        gnumake
-        gnused
-        graphviz
-        stable.haskellPackages.hledger_1_40
-        hunspellDicts.en_US-large
-        hunspellDicts.fr-any
-        hunspellDicts.fr-classique
-        hunspellDicts.fr-moderne
-        imagemagick
-        ispell
-        jq
-        multimarkdown # to preview markdown files
-        ncdu
-        p7zip
-        pandoc
-        pwgen
-        qpdf # to remove passwords from PDF
-        rsync
-        spice
-        sqlite
-        umlet
-        unar # universal unpacker
-        util-linux
-        unzip
-        wget
-        wmname
-        yaml-language-server
-        yarn-bash-completion
-        zbar # to scan QR codes for pass-otp
-      ]
-      ++ [
-        # Unfree packages
-        unrar
-        zip
-      ]
-      ++ [
-        # Fonts
-        aporetic
-        cantarell-fonts
-        dejavu_fonts
-        fira
-        fira-code
-        fira-mono
-        font-awesome
-        gentium
-        gentium-book-basic
-        inconsolata
-        iosevka-bin
-        jetbrains-mono
-        liberation_ttf
-        noto-fonts-emoji # recommended by Emacs
-        roboto-mono
-        ubuntu_font_family
-        xorg.fontmiscmisc
-      ]
-    );
+    ++ [
+      # Unfree packages
+      unrar
+      zip
+    ]
+    ++ [
+      # Fonts
+      aporetic
+      cantarell-fonts
+      dejavu_fonts
+      fira
+      fira-code
+      fira-mono
+      font-awesome
+      gentium
+      gentium-book-basic
+      inconsolata
+      iosevka-bin
+      jetbrains-mono
+      liberation_ttf
+      noto-fonts-emoji # recommended by Emacs
+      roboto-mono
+      ubuntu_font_family
+      xorg.fontmiscmisc
+    ]
+  );
 }
