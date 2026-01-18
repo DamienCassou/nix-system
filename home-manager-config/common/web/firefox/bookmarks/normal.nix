067254cdb1b8da0329ff805eae8c41f8950d5de9
Damien Cassou <damien@cassou.me>
2026-01-18 16:14:08 +0100

    firefox: format

--- home-manager-config/common/web/firefox/bookmarks/normal.nix
+++ home-manager-config/common/web/firefox/bookmarks/normal.nix
@@ -6,26 +6,22 @@ _: {
     }
     {
       name = "Wikipedia (en)";
-      url =
-        "https://en.wikipedia.org/w/index.php?title=Special%3ASearch&search=%s";
+      url = "https://en.wikipedia.org/w/index.php?title=Special%3ASearch&search=%s";
       keyword = "w";
     }
     {
       name = "Wikipedia (fr)";
-      url =
-        "https://fr.wikipedia.org/w/index.php?title=Sp%E9cial%3ARecherche&search=%s";
+      url = "https://fr.wikipedia.org/w/index.php?title=Sp%E9cial%3ARecherche&search=%s";
       keyword = "wfr";
     }
     {
       name = "English=>French";
-      url =
-        "https://www.wordreference.com/redirect/translation.aspx?dict=enfr&w=%s";
+      url = "https://www.wordreference.com/redirect/translation.aspx?dict=enfr&w=%s";
       keyword = "enfr";
     }
     {
       name = "French=>English";
-      url =
-        "https://www.wordreference.com/redirect/translation.aspx?dict=fren&w=%s";
+      url = "https://www.wordreference.com/redirect/translation.aspx?dict=fren&w=%s";
       keyword = "fren";
     }
     {
@@ -34,8 +30,7 @@ _: {
     }
     {
       name = "Horaires d'ouverture des médiathèques des Brest";
-      url =
-        "https://bibliotheque.brest-metropole.fr/iguana/www.main.cls?surl=brest_horaires";
+      url = "https://bibliotheque.brest-metropole.fr/iguana/www.main.cls?surl=brest_horaires";
     }
     {
       name = "Météo Brest";
