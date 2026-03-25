c1b4d45ee2e4735f47e4a3fce2b80e340837256b
Damien Cassou <damien@cassou.me>
2026-03-25 19:05:02 +0100

    raspberrypi: Add automatic garbage collection

--- machines/raspberrypi/nixos/default.nix
+++ machines/raspberrypi/nixos/default.nix
@@ -65,6 +65,11 @@
   };
 
   nix = {
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 30d";
+    };
     settings = {
       experimental-features = [
         "nix-command"
