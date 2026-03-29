fcfaa4a49fdb19daad86e0086d4c806d72f61782
Damien Cassou <damien@cassou.me>
2026-03-29 11:42:56 +0200

    raspberry: Move common nixos configuration to /nixos-config

--- machines/raspberrypi/nixos/default.nix
+++ machines/raspberrypi/nixos/default.nix
@@ -5,6 +5,7 @@
     ./filesystem.nix
     ./hardware-configuration.nix
     ./lacie.nix
+    ../../../nixos-config
   ];
 
   networking = {
@@ -13,80 +14,9 @@
       6600 # mpd
       8384 # syncthing
     ];
-    networkmanager.enable = true;
   };
-
-  time.timeZone = "Europe/Paris";
-
-  i18n = {
-    defaultLocale = "en_US.UTF-8";
-
-    extraLocaleSettings = {
-      LC_ADDRESS = "en_US.UTF-8";
-      LC_IDENTIFICATION = "en_US.UTF-8";
-      LC_MEASUREMENT = "en_US.UTF-8";
-      LC_MONETARY = "en_US.UTF-8";
-      LC_NAME = "en_US.UTF-8";
-      LC_NUMERIC = "en_US.UTF-8";
-      LC_PAPER = "en_US.UTF-8";
-      LC_TELEPHONE = "en_US.UTF-8";
-      LC_TIME = "en_US.UTF-8";
-    };
-  };
-
-  console.useXkbConfig = true; # use xkb setup in tty.
 
   security.rtkit.enable = true;
-
-  users = {
-    mutableUsers = false;
-    groups.cassou = {
-      gid = 1000;
-    };
-    users.cassou = {
-      isNormalUser = true;
-      home = "/Users/cassou";
-
-      # use mkpasswd to generate a hashedPassword:
-      hashedPassword = "$y$j9T$vcwc29rDt948CahE25xKu.$bc12.AHRsexSlhjMIU9WEuOIQXlJdkwBY1mxaPPquf.";
-
-      uid = 1000;
-      description = "Damien Cassou";
-      extraGroups = [
-        "audio"
-        "cassou"
-        "networkmanager"
-        "wheel"
-      ];
-      openssh.authorizedKeys.keys = [
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPNt/RcAiO+zgCvPUBXGHwPRr1qpufb/+tZlSab5D0cM cardno:000F_956772F4"
-      ];
-    };
-  };
-
-  nix = {
-    gc = {
-      automatic = true;
-      dates = "weekly";
-      options = "--delete-older-than 30d";
-    };
-    settings = {
-      experimental-features = [
-        "nix-command"
-        "flakes"
-      ];
-      trusted-users = [ "@wheel" ];
-    };
-  };
-
-  services = {
-    openssh.enable = true;
-
-    xserver.xkb = {
-      layout = "us";
-      variant = "colemak";
-    };
-  };
 
   system.stateVersion = "25.11";
 
